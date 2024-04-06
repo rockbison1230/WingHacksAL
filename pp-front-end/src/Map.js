adac5b5779e6f1b6aac308147c9d8ec90480ea1d
lily <164814255+trogoodkoe@users.noreply.github.com>
2024-04-06 12:41:40 +0000

    Pushing map to main repository

---
+++ pp-front-end/src/Map.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import USAMap from "react-usa-map";
+ 
+class App extends Component {
+  /* mandatory */
+  mapHandler = (event) => {
+    alert(event.target.dataset.name);
+  };
+ 
+  render() {
+    return (
+      <div className="App">
+        <USAMap onClick={this.mapHandler} />
+      </div>
+    );
+  }
+}
+ 
+export default App;
